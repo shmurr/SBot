f43dba35693dcda7476fd449c98bb57c9eda04be
shmurr <ArmandG@Armands-MacBook-Pro.local>
2016-10-30 11:12:43 +0100

    queue in progress

--- bot.js
+++ bot.js
@@ -3,16 +3,21 @@ const bot = new Discord.Client();
 const request = require("request");
 const ytdl = require("ytdl-core");
 nconf = require("nconf");
+var queue = [];
 
 //youtube
 bot.on('message', message => {
 	if(message.content.startsWith('!play')) {
-		var input = message.content.substring(6)
+		var input = message.content.substring(6);
+		queue.push(input);
 
 		const voiceChannel = message.member.voiceChannel;
 		voiceChannel.join()
 			.then(connection => {
-				const audio = connection.playStream(ytdl(input), {volume: "0.25"});
+				for(i = 0; i < queue.length; i++) {
+					const audio = connection.playStream(ytdl(queue[i]), {volume: "0.25"})};
+				message.channel.sendMessage('Now playing ' + input +' !')
+				console.log(bot.connection)
 				audio.on('end', () => {
 					voiceChannel.leave();
 				});
